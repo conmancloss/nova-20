64526fb40d2a387b2409b7d07a3c9add5f0ef375
conmancloss <thetechpro3@gmail.com>
2026-04-10 09:29:04 -0400

    Update bare server path in config.js

--- static/assets/history/config.js
+++ static/assets/history/config.js
@@ -5,7 +5,7 @@ self.__dynamic$config = {
 	logLevel: 0,
 	bare: {
 		version: 2,
-		path: "https://YOUR-BARE-SERVER.railway.app/",
+		path: "https://uv.holyubofficial.net/",
 	},
 	tab: {
 		title: null,
